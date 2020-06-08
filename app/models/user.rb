39a6c99dba5582351f1fd62e6f817b77fb0b4fcb
Leah Hughes <leahwhughes@gmail.com>
2020-06-08 13:52:20 -0700

    adds .build_from_github method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
   validates :provider, presence: true
   validates :uid, presence: true, uniqueness: true
 
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash[:uid]
+    user.provider = "github"
+    user.username = auth_hash["info"]["nickname"]
+    user.email = auth_hash["info"]["email"]
+
+    return user # unsaved!
+  end
 end
