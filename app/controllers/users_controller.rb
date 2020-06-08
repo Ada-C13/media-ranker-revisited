677d1fa87ede1242dee90cc92525ee53b7abe8bd
quinqu <quinruby0@gmail.com>
2020-06-08 13:24:49 -0700

    create user controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,18 @@ class UsersController < ApplicationController
     auth_hash = request.env["omniauth.auth"]
     user = User.find_by(uid: auth_hash[:uid], provider: "github")
     if user 
-      flash[:success] = "Logged in as returning user #{user.username}"
+      flash[:success] = "Welcome back, #{user.username}"
     else
-      if user
-      
+      user = User.create_user_from_github(auth_hash)
+      if user.save 
+        flash[:success] = "Welcome, #{user.username}"
+      else
+        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        return redirect_to root_path
+      end 
     end 
-
+    session[:user_id] = user.id
+    return redirect_to root_path
 
   end 
 
