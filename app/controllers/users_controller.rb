91d3a06ea2f9454593f547ecdd102b93c5c3019b
quinqu <quinruby0@gmail.com>
2020-06-08 13:36:48 -0700

    logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,36 +27,13 @@ class UsersController < ApplicationController
 
   end 
 
-  # def login_form
-  # end
 
-  # def login
-  #   username = params[:username]
-  #   if username and user = User.find_by(username: username)
-  #     session[:user_id] = user.id
-  #     flash[:status] = :success
-  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-  #   else
-  #     user = User.new(username: username)
-  #     if user.save
-  #       session[:user_id] = user.id
-  #       flash[:status] = :success
-  #       flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-  #     else
-  #       flash.now[:status] = :failure
-  #       flash.now[:result_text] = "Could not log in"
-  #       flash.now[:messages] = user.errors.messages
-  #       render "login_form", status: :bad_request
-  #       return
-  #     end
-  #   end
-  #   redirect_to root_path
-  # end
+  def destroy 
+    user = User.find_by(id: session[:user_id])
+    name = user.username 
+    session[:user_id] = nil
+    flash[:success] = "Logged out, bye #{name}! :)"
 
-  # def logout
-  #   session[:user_id] = nil
-  #   flash[:status] = :success
-  #   flash[:result_text] = "Successfully logged out"
-  #   redirect_to root_path
-  # end
+    redirect_to root_path
+  end 
 end
