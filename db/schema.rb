17f32de74b65887afc2f90419518b5e183174f62
Nikki Vaughan <vnikki13@gmail.com>
2020-06-08 16:59:10 -0700

    updated users model to reflect current tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_212931) do
+ActiveRecord::Schema.define(version: 2020_06_08_235721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_06_08_212931) do
     t.datetime "updated_at", null: false
     t.integer "uid"
     t.string "provider"
+    t.string "name"
+    t.string "email"
   end
 
   create_table "votes", id: :serial, force: :cascade do |t|
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,38 @@
 require "test_helper"
 
 describe UsersController do
+  describe 'auth_callback' do
+    it 'logs in an existing user and redirects to the root route' do
+      start_count = User.count
+      user = users(:grace)
+
+      perform_login(user)
+      must_redirect_to root_path
+      session[:user_id].must_equal user.id
+
+      User.count.must_equal start_count
+    end
+
+    it 'creates an account for a new user and redirects to the root route' do
+      start_count = User.count
+      user = User.new(provider: "github", uid: 99999, name: "test_user", email: "test@user.com")
+
+      perform_login(user)
+      must_redirect_to root_path
+      session[:user_id].must_equal User.last.id
+
+      User.count.must_equal start_count + 1
+    end
+
+    it 'redirects to root path if given invalid user data' do
+      start_count = User.count
+      user = User.new(provider: 'test', uid: -1)
+
+      perform_login(user)
+      must_redirect_to root_path
+      session[:user_id].must_equal nil
+
+      User.count.must_equal start_count
+    end
+  end
 end
