f41e65e05f1d857dba15ff9e143568e283853814
Hannah <hannahjoo24@gmail.com>
2020-06-08 14:53:39 -0700

    Update create and destroy action in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,31 +8,63 @@ class UsersController < ApplicationController
     render_404 unless @user
   end
 
-  def login_form
+  def create 
+    # This is information stored in the headers of the HTTP request that came back from GitHub.
+    auth_hash = request.env["omniauth.auth"]
+    
+    user = User.find_by(uid: auth_hash[:uid], provider: params[:provider])
+
+    if user 
+      flash[:success] = "Logged in as returning user #{user.username}"
+
+    else 
+      user = User.build_from_github(auth_hash)
+
+      if user.save 
+        flash[:success] = "Logged in as new user #{user.username}"
+      else
+        flash[:error] = "Could not create user account #{user.errors.messages}"
+        return redirect_to root_path
+      end
+    end 
+
+    session[:user_id] = user.id 
+    redirect_to root_path
+
   end
 
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else
-      user = User.new(username: username)
-      if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-      else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
-      end
-    end
+  def destroy 
+    session[:user_id] = nil 
+    flash[:success] = "Successfully logged out!"
+
     redirect_to root_path
   end
+
+  # def login_form
+  # end
+
+  # def login
+  #   username = params[:username]
+  #   if username and user = User.find_by(username: username)
+  #     session[:user_id] = user.id
+  #     flash[:status] = :success
+  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+  #   else
+  #     user = User.new(username: username)
+  #     if user.save
+  #       session[:user_id] = user.id
+  #       flash[:status] = :success
+  #       flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+  #     else
+  #       flash.now[:status] = :failure
+  #       flash.now[:result_text] = "Could not log in"
+  #       flash.now[:messages] = user.errors.messages
+  #       render "login_form", status: :bad_request
+  #       return
+  #     end
+  #   end
+  #   redirect_to root_path
+  # end
 
   def logout
     session[:user_id] = nil
