3ddbdd53da4fc0e988fb363c9e3785e9aa4b3476
Nikki Vaughan <vnikki13@gmail.com>
2020-06-08 14:53:48 -0700

    Updated logout action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "works#root"
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "users#create"
-  post "/logout", to: "users#logout", as: "logout"
+  delete "/logout", to: "user#destroy", as: "logout"
 
   resources :works
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
