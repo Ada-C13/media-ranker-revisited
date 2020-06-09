943539a042106bf4c5decf473733646ab757c8dd
Sharon Cheung <keikei1128@g.ucla.edu>
2020-06-08 23:29:49 -0700

    added guest user log out test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,10 +43,17 @@ describe UsersController do
       post logout_path
 
       expect(session[:user_id]).must_be_nil
+      expect(flash[:status]).must_equal :success
+      expect(flash[:result_text]).must_equal "Successfully logged out"
       must_redirect_to root_path
     end
 
-    it "guest users won't be able to log out without logged in "
+    it "guest users won't be able to log out without logged in " do
+      post logout_path
+      
+      expect(flash[:status]).must_equal :failure
+      expect(flash[:result_text]).must_equal "You must be logged in before to logout!"
+      must_redirect_to root_path
     end
   end
 end
