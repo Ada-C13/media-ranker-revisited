b0f6afe33f64641bda7e9d5999f245aa8056af9b
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-09 00:13:31 -0700

    adds validations for uid and email to user model, plus testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :destroy
   has_many :ranked_works, through: :votes, source: :work
 
-  validates :username, uniqueness: true, presence: true
+  validates :username, :uid, :email, uniqueness: true, presence: true
 
   def self.build_from_github(auth_hash)
     user = User.new
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,40 +3,71 @@ require "test_helper"
 describe User do
   describe "relations" do
     it "has a list of votes" do
-      dan = users(:dan)
-      expect(dan).must_respond_to :votes
-      dan.votes.each do |vote|
+      user = users(:user1)
+      expect(user).must_respond_to :votes
+      user.votes.each do |vote|
         expect(vote).must_be_kind_of Vote
       end
     end
 
     it "has a list of ranked works" do
-      dan = users(:dan)
-      expect(dan).must_respond_to :ranked_works
-      dan.ranked_works.each do |work|
+      user = users(:user1)
+      expect(user).must_respond_to :ranked_works
+      user.ranked_works.each do |work|
         expect(work).must_be_kind_of Work
       end
     end
   end
 
   describe "validations" do
+    before do
+      @user = User.new(
+        uid: "0",
+        username: "newuser",
+        email: "newuser@test.com"
+      )
+    end
+
     it "requires a username" do
-      user = User.new
-      expect(user.valid?).must_equal false
-      expect(user.errors.messages).must_include :username
+      @user.username = nil
+      expect(@user.valid?).must_equal false
+      expect(@user.errors.messages).must_include :username
     end
 
     it "requires a unique username" do
-      username = "test username"
-      user1 = User.new(username: username)
-
-      # This must go through, so we use create!
-      user1.save!
-
-      user2 = User.new(username: username)
-      result = user2.save
+      @user.username = users(:user1).username
+      
+      result = @user.save
       expect(result).must_equal false
-      expect(user2.errors.messages).must_include :username
+      expect(@user.errors.messages).must_include :username
+    end
+
+    it "requires a uid" do
+      @user.uid = nil
+      expect(@user.valid?).must_equal false
+      expect(@user.errors.messages).must_include :uid
+    end
+
+    it "requires a unique uid" do
+      @user.uid = users(:user1).uid
+      
+      result = @user.save
+      expect(result).must_equal false
+      expect(@user.errors.messages).must_include :uid
+    end
+
+    it "requires an email" do
+      @user.email = nil
+      expect(@user.valid?).must_equal false
+      expect(@user.errors.messages).must_include :email
+    end
+
+    it "requires a unique email" do
+      @user.email = users(:user1).email
+      
+      result = @user.save
+      expect(result).must_equal false
+      expect(@user.errors.messages).must_include :email
     end
   end
 end
