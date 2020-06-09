d72552b92bf30401d87d27266fa59e0f3886172a
Sharon Cheung <keikei1128@g.ucla.edu>
2020-06-08 22:35:34 -0700

    add user fixtures

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,11 @@
 require "test_helper"
 
 describe UsersController do
+  describe 'login' do 
+    it 'can log in an existing user' do
+      user = perform_login(users(:dee))
+
+      must_respond_with :redirect
+    end
+  end
 end
