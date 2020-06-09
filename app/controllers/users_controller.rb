46e3419d5ee063aeea2f7cd23952d9c1ed153abd
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-08 23:36:47 -0700

    reconfigs login/logout routes, adds create action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,22 @@ class UsersController < ApplicationController
     render_404 unless @user
   end
 
-  def login_form
-  end
+  def create
+    auth_hash = request.env["omniauth.auth"]
+    user = User.find_by(uid: auth_hash[:uid], provider: "github")
 
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
+    if user
       flash[:status] = :success
       flash[:result_text] = "Successfully logged in as existing user #{user.username}"
     else
-      user = User.new(username: username)
+      user = User.new(
+        uid: auth_hash[:uid], 
+        provider: auth_hash[:provider], 
+        username: auth_hash[:info][:nickname], 
+        name: auth_hash[:info][:name], 
+        email: auth_hash[:info][:email]
+      )
       if user.save
-        session[:user_id] = user.id
         flash[:status] = :success
         flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
       else
@@ -31,7 +34,10 @@ class UsersController < ApplicationController
         return
       end
     end
+
+    session[:user_id] = user.id
     redirect_to root_path
+    return
   end
 
   def logout
