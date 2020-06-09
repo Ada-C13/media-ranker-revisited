6cc7ba08f5cd9f287139b9beb9a1d8b70d0aa8a8
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-09 07:11:42 -0700

    Added test for creating a new user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,16 @@ describe UsersController do
     end
 
     it "creates an account for a new user and redirects to the root route" do
-      
+      start_count = User.count
+      user = User.new(provider: "github", uid: 99999, username: "test_user", email: "test@user.com")
+    
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+      get auth_callback_path(:github)
+    
+      must_redirect_to root_path
+    
+      User.count.must_equal start_count + 1
+      session[:user_id].must_equal User.last.id
     end
 
     it "redirects to the login route if given invalid user data" do
