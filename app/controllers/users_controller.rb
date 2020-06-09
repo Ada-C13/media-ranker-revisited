abac4c41647c0c86734305ab4844d4f3505cddc5
Sharon Cheung <keikei1128@g.ucla.edu>
2020-06-08 19:44:03 -0700

    modiify user model & controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+  def create
+    auth_hash = request.env["omniauth.auth"]
+
+    user = User.find_by(ud: auth_hash[:uid],
+                        provider: auth_hash[:provider])
+    
+    if user 
+      flash[:notice] = "Existing user #{user.username} is logged in"
+    else
+      user = User.build_from_github(auth_hash)
+      if user.save
+        flash[:success] = "Logged in as new user #{user.username}"
+      else
+        flash[:error] = "Couldn't create user account #{user.errors.message}"
+        return redirect_to root_path
+      end
+    end
+
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
+
   def index
     @users = User.all
   end
