0b81bc8a363bf117fbc5c32a88081e68ab63652c
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-09 07:18:15 -0700

    Refactored tests by pulling out perform_login helper

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,11 +7,9 @@ describe UsersController do
       start_count = User.count
       user = users(:dan)
 
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-      get auth_callback_path(:github)
-
+      perform_login(user)
+      
       must_redirect_to root_path
-
       session[:user_id].must_equal user.id
       User.count.must_equal start_count
     end
@@ -20,11 +18,9 @@ describe UsersController do
       start_count = User.count
       user = User.new(provider: "github", uid: 99999, username: "test_user", email: "test@user.com")
     
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-      get auth_callback_path(:github)
-    
+      perform_login(user)
+
       must_redirect_to root_path
-    
       User.count.must_equal start_count + 1
       session[:user_id].must_equal User.last.id
     end
