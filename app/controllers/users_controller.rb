75c76ca966edc57e3b7b696d38c1fb457f7b2b6d
thenora <thenora@gmail.com>
2020-06-08 18:21:59 -0700

    login info updated in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,9 +49,23 @@ class UsersController < ApplicationController
       flash[:success] = "Logged in as returning user #{user.username}"
     else
       # User doesn't match anything in the DB
-      # TODO: Attempt to create a new user
+      # Attempt to create a new user
+      user = User.build_from_github(auth_hash)
+
+      if user.save
+        flash[:success] = "Logged in as new user #{user.username}"
+      else
+        # Couldn't save the user for some reason. If we
+        # hit this it probably means there's a bug with the
+        # way we've configured GitHub. Our strategy will
+        # be to display error messages to make future
+        # debugging easier.
+        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        return redirect_to root_path
+      end
     end
 
+    # If we get here, we have a valid user instance
     session[:user_id] = user.id
     redirect_to root_path
   end
