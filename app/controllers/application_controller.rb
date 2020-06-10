5d92b865323b7b07fb1516e882137baba4017e2c
quinqu <quinruby0@gmail.com>
2020-06-09 18:35:14 -0700

    user authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :find_user
+
 
   def render_404
     # DPR: this will actually render a 404 page in production
@@ -13,6 +13,14 @@ class ApplicationController < ActionController::Base
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
+    end
+    return @login_user
+  end
+
+  def require_login
+    if @login_user.nil?
+        flash[:error] = "You must log in to do that"
+      redirect_to root_path
     end
   end
 end
