4f7e9f8997ca1b07333fd3a487670f57e383e2e1
Rosalyn <syrosalynyu@gmail.com>
2020-06-09 22:24:33 -0700

    controller tests for user logout action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -16,4 +16,18 @@ describe UsersController do
       expect(flash[:notice]).must_equal "Welcome, #{new_user.username}!"
     end
   end
+
+  describe "logout" do
+    it "can logout as an existing user" do
+      user = perform_login(users(:dan))
+
+      expect(session[:user_id]).wont_be_nil
+
+      post logout_path, params: {}
+
+      expect(session[:user_id]).must_be_nil
+      expect(flash[:notice]).must_equal "Successfully logged out"
+      must_redirect_to root_path
+    end
+  end
 end
