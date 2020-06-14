7e86120af5c8b146b2720bd5944f4450df464bce
Yesenia <yesentorres@gmail.com>
2020-06-14 15:30:40 -0700

    added create user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,30 @@ class UsersController < ApplicationController
     render_404 unless @user
   end
 
+  def create
+    auth_hash = request.env["omniauth.auth"]
+
+    user = User.find_by(uid: auth_hash[:uid], provider: "github")
+    if user
+      # user was found in the database
+      flash[:success] = "Logged in as returning user #{user.username}"
+    else
+      # user not found in db, attempt to create a new user
+      user = User.build_from_github(auth_hash)
+
+      if user.save
+        flash[:success] = "Logged in as new user #{user.username}"
+      else
+        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        return redirect_to root_path
+      end
+    end
+
+    # store logged in user in session
+    session[:user_id] = user.id
+    return redirect_to root_path
+  end
+
   def login_form
   end
 
