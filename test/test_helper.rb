3f72354f0c01ff0c7e268bdfc8cbfa70df59e496
Yesenia <yesentorres@gmail.com>
2020-06-14 18:14:33 -0700

    move helper methods within active support class

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,25 +22,35 @@ Minitest::Reporters.use!(
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  # Add more helper methods to be used by all tests here...
-end
 
-def setup
-  # Once you have enabled test mode, all requests
-  # to OmniAuth will be short circuited to use the mock authentication hash.
-  # A request to /auth/provider will redirect immediately to /auth/provider/callback.
-  OmniAuth.config.test_mode = true
-end
-
-def mock_auth_hash(user)
-  # Test helper method to generate a mock auth hash
-  # for fixture data
-  return {
-    provider: user.provider,
-    uid: user.uid,
-    info: {
-      email: user.email,
-      nickname: user.name
+  def setup
+    # Once you have enabled test mode, all requests
+    # to OmniAuth will be short circuited to use the mock authentication hash.
+    # A request to /auth/provider will redirect immediately to /auth/provider/callback.
+    OmniAuth.config.test_mode = true
+  end
+  
+  def mock_auth_hash(user)
+    # Test helper method to generate a mock auth hash
+    # for fixture data
+    return {
+      provider: user.provider,
+      uid: user.uid,
+      info: {
+        email: user.email,
+        nickname: user.name
+      }
     }
-  }
+  end
+  
+  def perform_login(user = nil)
+    user ||= User.first
+  
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    get auth_callback_path(:github)
+  
+    return user
+  end
 end
+
+
