451c42b1fafc101ab5ea5bbc32ed337ab5228263
vwhwang <vwhwang38@gmail.com>
2020-06-18 15:30:19 -0700

    added a few controller filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+  
 
   def render_404
     # DPR: this will actually render a 404 page in production
     raise ActionController::RoutingError.new("Not Found")
+  end
+
+
+  def require_login
+    if find_user.nil?
+      flash.now[:error] = "You must be logged in to view this section"
+      redirect_to root_path
+    end
   end
 
   private
