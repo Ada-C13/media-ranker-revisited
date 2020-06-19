d911b0f5367989339b5082f492914c03288b0f8c
vwhwang <vwhwang38@gmail.com>
2020-06-19 10:48:29 -0700

    added some controller filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :find_user
+  before_action :find_user, :require_login
   
 
   def render_404
