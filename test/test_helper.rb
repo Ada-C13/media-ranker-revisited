60c531a9b41632edb20f42853fb5bffdbc780d87
thenora <thenora@gmail.com>
2020-06-19 01:03:49 -0700

    too many things

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,4 +23,19 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
+  before_action :require_login
+  before_action :find_user
+  
+  def current_user
+    return User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  
+  def require_login
+    if current_user.nil?
+      flash[:error] = "Oops. Looks like you don't have permission to view this page. Please login or create an account."
+      redirect_to root_path
+    end
+  end
+
+
 end
