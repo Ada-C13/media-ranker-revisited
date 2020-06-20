6f137aca1cb3c240155626463f73eb3062170e96
Catherina <catherine.pchelina@gmail.com>
2020-06-20 15:55:09 -0700

    Add tests for build_from_github method

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,4 +39,33 @@ describe User do
       expect(user2.errors.messages).must_include :username
     end
   end
+
+  describe "custom methods" do 
+    describe "build_from_github" do 
+      it "is invalid without username" do 
+        user = User.new(
+          provider: "github",
+          uid: 3875394857,
+          email: "test@gmail.com",
+        )
+
+        expect(user.valid?).must_equal false 
+        expect(user.errors.messages).must_include :username
+      end
+
+      it "username must be unique" do 
+        user_one = users(:dan)
+        user_two = User.new(
+          provider: "github",
+          uid: 3875394857,
+          email: "test@gmail.com",
+          username: "dan"
+        )
+
+        user_two.save
+        expect(user_two.valid?).must_equal false 
+        expect(user_two.errors.messages).must_include :username
+      end
+    end
+  end
 end
