013aaf3e0afd50af92d69049fb4871dece2d4d55
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-06-21 15:30:47 -0700

    Drop off name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_224055) do
+ActiveRecord::Schema.define(version: 2020_06_21_183042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2020_06_20_224055) do
     t.integer "uid"
     t.string "provider"
     t.string "email"
-    t.string "name"
   end
 
   create_table "votes", id: :serial, force: :cascade do |t|
