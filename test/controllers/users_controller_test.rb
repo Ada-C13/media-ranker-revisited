9681f2cc6a96bc26502018c8ea0452c2b0f39554
Angela Nguyen <angethuy@gmail.com>
2020-06-20 18:42:29 -0700

    scaffolding for users controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,35 @@
 require "test_helper"
 
 describe UsersController do
+  describe "index" do
+    it "successfully retrieves index" do
+    end
+  end
+
+  describe "show" do
+    it "successfully retrieves show pages" do
+    end
+
+    it "errors when retrieving a page for a nonexistent product" do
+    end
+  end
+
+  describe "create" do
+    it "logs in a new user" do
+    end
+
+    it "logs in a returning user" do
+    end
+
+    it "does not create a user account for malformed github requests" do
+    end
+  end
+
+  describe "logout" do
+    it "removes the user from session" do
+    end
+
+    it "flashes error message when no user is logged in" do
+    end
+  end
 end
