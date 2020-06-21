907c9ef383aa588b28e754e51f1e0a2cc10aaedf
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-06-21 02:18:03 -0700

    removed simple login in and changed to oauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:create]
+
   def index
     @users = User.all
   end
@@ -6,32 +8,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
     render_404 unless @user
-  end
-
-  def login_form
-  end
-
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else
-      user = User.new(username: username)
-      if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-      else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
-      end
-    end
-    redirect_to root_path
   end
 
   def create
@@ -42,18 +18,11 @@ class UsersController < ApplicationController
       # User was found in the database
       flash[:success] = "Logged in as returning user #{user.username}"
     else
-      # User doesn't match anything in the DB
-      # Attempt to create a new user
       user = User.build_from_github(auth_hash)
 
       if user.save
         flash[:success] = "Logged in as new user #{user.username}"
       else
-        # Couldn't save the user for some reason. If we
-        # hit this it probably means there's a bug with the
-        # way we've configured GitHub. Our strategy will
-        # be to display error messages to make future
-        # debugging easier.
         flash[:error] = "Could not create new user account: #{user.errors.messages}"
         return redirect_to root_path
       end
@@ -61,13 +30,20 @@ class UsersController < ApplicationController
 
     # If we get here, we have a valid user instance
     session[:user_id] = user.id
+    session[:username] = user.username
     return redirect_to root_path
   end
 
-  def logout
-    session[:user_id] = nil
-    flash[:status] = :success
-    flash[:result_text] = "Successfully logged out"
-    redirect_to root_path
+  def destroy
+    if session[:user_id]
+      session[:user_id] = nil
+      flash[:success] = "Successfully logged out!"
+      redirect_to root_path
+      return
+    else
+      flash[:failure] = "You are not logged on"
+      redirect_to root_path
+      return
+    end
   end
 end
