484575642fb1b546488452a77c745100bc71fc73
Hannah <hannahjoo24@gmail.com>
2020-06-21 01:49:53 -0700

    Update users_controller_test.rb & user_test.rb

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,6 +5,9 @@ describe UsersController do
     it "can log in as existing user" do 
       user = perform_login(users(:ada))
 
+      expect(flash["status"]).must_equal :success 
+      expect(flash["result_text"]).must_include "Logged in as returning user"
+
       must_respond_with :redirect
     end
 
@@ -14,6 +17,9 @@ describe UsersController do
       expect {
         logged_in_user = perform_login(new_user)
       }.must_change "User.count", 1
+
+      expect(flash["status"]).must_equal :success 
+      expect(flash["result_text"]).must_include "Logged in as new user"
 
       must_respond_with :redirect
     end
@@ -27,6 +33,9 @@ describe UsersController do
         perform_login(new_user)
       }.wont_change "User.count"
 
+      expect(flash["status"]).must_equal :failure
+      expect(flash["result_text"]).must_include "Could not create user account"
+
       must_redirect_to root_path
     end
   end
@@ -38,22 +47,23 @@ describe UsersController do
 
       expect(session[:user_id]).wont_be_nil
 
-      delete logout_path, params: {} 
+      delete logout_path
 
       expect(session[:user_id]).must_be_nil 
+
+      expect(flash["status"]).must_equal :success
+      expect(flash["result_text"]).must_include "Successfully logged out!"
+
       must_redirect_to root_path
     end
 
     # TODO
-    it "guest users on the route" do 
+    # it "guest users on the route" do 
       
-      delete logout_path, params: {} 
+    #   delete logout_path
 
-      expect(session[:user_id]).must_be_nil 
-      must_redirect_to root_path
-
-    end
+    #   expect(session[:user_id]).must_be_nil 
+    #   must_redirect_to root_path
+    # end
   end
-
-
 end
