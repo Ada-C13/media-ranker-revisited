276dc3ea1c1677b3ce06895a52c3d924cd6ed272
cojenco <icathyouyang@gmail.com>
2020-06-21 00:05:38 -0700

    add tests for works#upvote

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -188,20 +188,49 @@ describe WorksController do
   end
 
   describe "upvote" do
-    it "redirects to the work page if no user is logged in" do
-      skip
+    before do
+      @poodr = works(:poodr)
     end
 
-    it "redirects to the work page after the user has logged out" do
-      skip
+    it "redirects to the work page if no user is logged in" do      
+      expect{
+        post upvote_path(@poodr.id)
+      }.wont_change "Vote.count"
+
+      must_respond_with :redirect
+      expect(flash[:result_text]).must_equal "You must log in to do that" 
+    end
+
+    it "redirects to the work page after the user has upvoted" do
+      perform_login
+      post upvote_path(@poodr.id)
+
+      must_respond_with :redirect
+      must_redirect_to work_path(@poodr)
     end
 
     it "succeeds for a logged-in user and a fresh user-vote pair" do
-      skip
+      perform_login
+      expect{
+        post upvote_path(@poodr.id)
+      }.must_differ "Vote.count", 1
+
+      must_respond_with :redirect
+      expect(flash[:status]).must_equal :success     
     end
 
     it "redirects to the work page if the user has already voted for that work" do
-      skip
+      perform_login
+      expect{
+        post upvote_path(@poodr.id)
+      }.must_differ "Vote.count", 1
+
+      expect{
+        post upvote_path(@poodr.id)
+      }.wont_change "Vote.count"
+
+      must_respond_with :redirect
+      must_redirect_to work_path(@poodr)   
     end
   end
 end
