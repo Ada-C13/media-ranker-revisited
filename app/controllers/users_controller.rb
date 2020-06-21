065b60d5fa4d4ab1fb1a4cd1b2a80f51faa2c0ff
Angela Nguyen <angethuy@gmail.com>
2020-06-20 18:05:30 -0700

    added User action to detect or create users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,24 @@ class UsersController < ApplicationController
 
   def create
     auth_hash = request.env["omniauth.auth"]
-    raise
+
+    user = User.find_by(uid: auth[:uid], provider: "github")
+
+    if user 
+      flash[:success] = "Logged in as returning user #{user.username}"
+    else 
+      user = User.build_from_github(auth_hash)
+
+      if user.save
+        flash[:success] = "Logged in as new user #{user.username}"
+      else
+        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        return redirect_to root_path
+      end
+
+      session[:user_id] = user.id
+      return redirect_to root_path
+    end
   end
 
   # def login_form
