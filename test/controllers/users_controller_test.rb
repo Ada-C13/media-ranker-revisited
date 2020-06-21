384f5e8b2302917c31351b64d152305ae30b69a6
Jessica Stone <jstone4815@gmail.com>
2020-06-20 17:51:39 -0700

    refactor tests using perform_login helper

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,37 @@
 require "test_helper"
 
 describe UsersController do
+  describe "auth_callback" do
+    # test/controllers/users_controller_test.rb
+    it "logs in an existing user" do
+      start_count = User.count
+      user = users(:grace)
+
+      perform_login(user)
+      must_redirect_to root_path
+      expect(session[:user_id]).must_equal user.id
+
+      # Should *not* have created a new user
+      expect(User.count).must_equal start_count
+    end
+
+    it "creates a new user" do
+      start_count = User.count
+      user = User.new(provider: "github", uid: 99999, username: "test_user", email: "test@user.com")
+    
+      perform_login(user)
+      get auth_callback_path(:github)
+    
+      must_redirect_to root_path
+    
+      # Should have created a new user
+      expect(User.count).must_equal start_count + 1
+    
+      # The new user's ID should be set in the session
+      expect(session[:user_id]).must_equal User.last.id
+    end
+
+    it "redirects to the login route if given invalid user data" do
+    end
+  end  
 end
