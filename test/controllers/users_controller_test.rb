7ef12aa3c8ff48e7a90c16ba02f65170949a9a19
mulhoo <oliviamulholland94@gmail.com>
2020-06-22 15:19:33 -0700

    user tests pass, some minor fixes

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ describe UsersController do
       user = users(:dw)
       login(user)
 
-      expect(flash[:success]).must_equal "Logged in as returning user #{user.name}"
+      expect(flash[:success]).must_equal "Logged in as returning user #{user.username}"
       session[:user_id].must_equal user.id
       must_redirect_to root_path
       User.count.must_equal count
@@ -28,21 +28,21 @@ describe UsersController do
       user = User.new(provider: "github", uid: 12347, username: "hannah_montana", email: "hannah_montana@montana.com", name: "Hannah")
       login(user)
       
-      expect(flash[:success]).must_equal "Logged in as new user #{user.name}"
+      expect(flash[:success]).must_equal "Logged in as new user #{user.username}"
       session[:user_id].must_equal User.last.id
       must_redirect_to root_path
-      User.count.must_equal start_count + 1
+      User.count.must_equal count + 1
     end
 
     it "redirects to login route if given bad data" do
       count = User.count
       bad_data = User.create(provider: "github", uid: 1232)
-      login(user)
+      login(bad_data)
 
-      expect(flash[:error]).must_include "Could not create new user account: "
+      expect(flash[:error]).must_include "could not create new user account: #{bad_data.errors.messages}"
       must_redirect_to root_path
       assert_nil(session[:user_id])
-      User.count.must_equal start_count
+      User.count.must_equal count
     end
   end
 
@@ -59,6 +59,32 @@ describe UsersController do
       get user_path(invalid_id)
       
       must_respond_with :not_found
+    end
+  end
+
+  describe "destroy" do
+    it "logs out valid user and redirects" do
+      count = User.count
+      user = users(:arthur)
+
+      login(user)
+      delete logout_path
+
+      assert_nil(session[:user_id])
+      expect(flash[:success]).must_equal "Successfully logged out!"
+      User.count.must_equal count 
+      must_respond_with :redirect
+      must_redirect_to root_path     
+    end
+
+    it "redirects to root if you try to log out, but nobody is logged in" do
+      count = User.count  
+      delete logout_path
+      
+      assert_nil(session[:user_id])
+      User.count.must_equal count
+      must_respond_with :redirect
+      must_redirect_to root_path      
     end
   end
 end
