bce8bb1d3464bf4efc8b79cb26bbb0c6a232ba82
Jocelyn Wang <wangjoc@gmail.com>
2020-06-22 05:19:29 -0700

    added in require login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,14 @@ class ApplicationController < ActionController::Base
     raise ActionController::RoutingError.new("Not Found")
   end
 
+  def require_login
+    if @login_user.nil?
+      flash[:error] = "You must be logged in."
+      redirect_to root_path
+      # return "You must be logged in."
+    end
+  end
+
   private
 
   def find_user
