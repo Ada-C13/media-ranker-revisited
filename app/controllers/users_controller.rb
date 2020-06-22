96af461a9bf15f5db46f4bba93750bceffd329a0
Alex Robertson <arobertson335@gmail.com>
2020-06-21 20:14:20 -0700

    Adds create action for Users Controller and helper method in User model.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,25 @@ class UsersController < ApplicationController
     render_404 unless @user
   end
 
+
   def create
     auth_hash = request.env["omniauth.auth"]
-    raise
+
+    user = User.find_by(uid: auth_hash[:uid], provider: "github")
+    if user
+      flash[:success] = "Logged in as returning user #{user.username}"
+    else
+      user = User.build_from_github(auth_hash)
+      if user.save
+        flash[:success] = "Logged in as new user #{user.username}"
+      else
+        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        return redirect_to root_path
+      end
+    end
+
+    session[:user_id] = user.id
+    return redirect_to root_path
   end
 
   # def login_form
