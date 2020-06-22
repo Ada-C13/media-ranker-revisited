5d746a473f9d0461964dba6e44607775d9fe2610
Rosalyn <syrosalynyu@gmail.com>
2020-06-21 22:28:35 -0700

    add required_login action in application controlle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :find_user
+  before_action :find_user, :required_login
 
   def render_404
     # DPR: this will actually render a 404 page in production
@@ -13,6 +13,14 @@ class ApplicationController < ActionController::Base
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def required_login
+    if !find_user
+      flash[:status] = :failure
+      flash[:result_text] = "You must be logged in to do that"
+      redirect_to root_path
     end
   end
 end
