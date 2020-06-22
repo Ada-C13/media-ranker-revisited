3b071eff56f28c99728af07b4d4e6edad5d62425
Jocelyn Wang <wangjoc@gmail.com>
2020-06-22 02:54:56 -0700

    added in new columns to user table

---
+++ db/migrate/20200622094136_add_columns_to_user.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :uid, :integer
+    add_column :users, :provider, :string
+    add_column :users, :email, :string
+    add_column :users, :avatar, :string
+  end
+end
