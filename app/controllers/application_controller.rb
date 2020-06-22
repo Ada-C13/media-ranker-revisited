0623786cca52acb2470ec3a6ea15ae618e313181
Olga Patrakova <olgapatrakova@outlook.com>
2020-06-21 19:30:56 -0700

    OAuth create action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,19 @@ class ApplicationController < ActionController::Base
 
   before_action :find_user
 
+  def current_user
+    return @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def require_login
+    if current_user.nil?
+      flash[:status] = :failure 
+      flash[:result_text] = "You must be logged in to do that"
+
+      redirect_to root_path
+    end
+  end
+
   def render_404
     # DPR: this will actually render a 404 page in production
     raise ActionController::RoutingError.new("Not Found")
