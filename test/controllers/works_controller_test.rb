c3a9dc4efdf84bbaa510fa7bfb02b8dd2b9d0fee
mulhoo <oliviamulholland94@gmail.com>
2020-06-22 15:44:15 -0700

    works controller updates for W3

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -188,20 +188,24 @@ describe WorksController do
   end
 
   describe "upvote" do
-    it "redirects to the work page if no user is logged in" do
-      skip
+    describe "guest user" do
+      it "redirects to the work page if no user is logged in" do
+        skip
+      end
+
+      it "redirects to the work page after the user has logged out" do
+        skip
+      end
     end
 
-    it "redirects to the work page after the user has logged out" do
-      skip
-    end
+    describe "authorized user" do
+      it "succeeds for a logged-in user and a fresh user-vote pair" do
+        skip
+      end
 
-    it "succeeds for a logged-in user and a fresh user-vote pair" do
-      skip
-    end
-
-    it "redirects to the work page if the user has already voted for that work" do
-      skip
+      it "redirects to the work page if the user has already voted for that work" do
+        skip
+      end
     end
   end
 end
