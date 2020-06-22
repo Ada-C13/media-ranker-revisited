d319aa0461206f55960092b824b10948d46fdd1c
dnguye2 <dnguyen1093@gmail.com>
2020-06-21 22:48:39 -0700

    added requrie_login before_action filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :find_user
 
   def render_404
     # DPR: this will actually render a 404 page in production
     raise ActionController::RoutingError.new("Not Found")
   end
+  
+  def current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    if current_user.nil?
+      flash[:status] = :failure
+      flash[:result_text] = "You must be logged in to do that"
+      redirect_to root_path
+    end
+  end
+
 
   private
 
