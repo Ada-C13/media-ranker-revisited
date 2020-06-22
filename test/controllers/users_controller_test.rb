07b46f7af6f1c664e8cdd208960b622ed2744348
Alex Robertson <arobertson335@gmail.com>
2020-06-21 23:18:06 -0700

    Adds user controller tests and fixtures.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,33 @@
 require "test_helper"
 
 describe UsersController do
+
+  describe 'login' do
+    it 'allows an existing user to login' do
+      user = perform_login(users(:devin))
+
+      must_respond_with :redirect
+    end
+
+    it 'enables a new user to log in' do
+      new_user = User.new(username: 'kaida', provider: 'github', uid: 3632)
+
+      expect {
+        logged_in_user = perform_login(new_user)
+      }.must_change "User.count", 1
+      must_respond_with :redirect
+    end
+  end
+
+  describe "logout" do 
+    it "can logout an existing user" do 
+      expect {
+        perform_logout
+      }.wont_change "User.count"
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+      expect(session[:user_id]).must_be_nil
+    end 
+  end
 end
