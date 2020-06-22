53db634104fb9f605bec1eb754e9713273cfc0f3
Katie <katie.vandervoort@gmail.com>
2020-06-21 22:15:38 -0700

    fixed failing tests that weren't using uid for new users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,37 @@
 require "test_helper"
 
 describe UsersController do
+  describe "auth_callback" do
+    it "logs in an existing user and redirects to the root route" do
+
+      start_count = User.count
+      user = users(:grace)
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+      get auth_callback_path(:github)
+
+      must_redirect_to root_path
+      session[:user_id].must_equal user.id
+      User.count.must_equal start_count
+    end
+
+    it "creates a new user" do
+      start_count = User.count
+      user = User.new(provider: "github", uid: 99999, name: "test_user", email: "test@user.com")
+    
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+      get auth_callback_path(:github)
+    
+      must_redirect_to root_path
+    
+      # Should have created a new user
+      User.count.must_equal start_count + 1
+    
+      # The new user's ID should be set in the session
+      session[:user_id].must_equal User.last.id
+    end
+
+    it "redirects to the login route if given invalid user data" do
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,7 +26,7 @@ class ActiveSupport::TestCase
   def setup
     OmniAuth.config.test_mode = true
   end
-  
+
   def mock_auth_hash(user)
     return {
       provider: user.provider,
@@ -37,4 +37,13 @@ class ActiveSupport::TestCase
       }
     }
   end
+  def perform_login(user = nil)
+    user ||= User.first
+  
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    get auth_callback_path(:github)
+  
+    return user
+  end
+
 end
