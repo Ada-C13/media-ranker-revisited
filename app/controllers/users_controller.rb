88adc2499b95541525b5a143c4742c885f8cf179
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-22 07:00:17 -0700

    removes .now from flash messages, unneeded due to redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,9 @@ class UsersController < ApplicationController
         flash[:status] = :success
         flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
       else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
+        flash[:status] = :failure
+        flash[:result_text] = "Could not log in"
+        flash[:messages] = user.errors.messages
         redirect_to root_path
         return
       end
