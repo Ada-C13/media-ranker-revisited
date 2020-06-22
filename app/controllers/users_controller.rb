c6c7b4e52d852f577c3f8afc02a093e1f6104d25
Alex Robertson <arobertson335@gmail.com>
2020-06-21 20:23:31 -0700

    Adds destroy action for User controller, route, and edits view buttons for log-in/log-out.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,15 @@ class UsersController < ApplicationController
     return redirect_to root_path
   end
 
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out!"
+
+    redirect_to root_path
+  end
+
+  
+
   # def login_form
   # end
 
