3cd0bf9839fcdf65d7c6bf33aa9f9a67d8d075be
Yieni <yaynee8d@gmail.com>
2020-06-22 15:18:52 -0700

    add users_controller test for login existing user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,26 @@
 require "test_helper"
 
 describe UsersController do
+  describe "auth_callback" do
+    it "logs in an existing user and redirects to the root route" do
+      user_count = User.count
+
+      user = users(:grace)
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+      get auth_callback_path(:github)
+
+      must_redirect_to root_path
+
+      session[:user_id].must_equal user.uid
+
+      User.count.must_equal user_count
+    end
+
+    it "creates an account for a new user and redirects to the root route" do
+    end
+
+    it "redirects to the login route if given invalid user data" do
+    end
+  end
 end
